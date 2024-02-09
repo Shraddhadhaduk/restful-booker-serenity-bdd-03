29394d7b484e432d5e7e654739a100c10377f805
Shraddhadhaduk <shraddhadhaduk96@gmail.com>
2024-02-09 00:57:43 +0000

    restful booker serenity 03

---
+++ src/test/java/com/restful/booker/cucumber/CucumberRunner.java
@@ -0,0 +1,11 @@
+package com.restful.booker.cucumber;
+
+import com.restful.booker.testbase.TestBase;
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/resources/features/")
+public class CucumberRunner extends TestBase {
+}
